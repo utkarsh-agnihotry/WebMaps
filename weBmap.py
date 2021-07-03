363616af6069aa146f0d879d3c439ea1b29e8650
utkarsh-agnihotry <47258275+utkarsh-agnihotry@users.noreply.github.com>
2021-07-03 17:45:21 +0530

    Add files via upload

---
+++ weBmap.py
@@ -0,0 +1,26 @@
+import folium
+import pandas
+data=pandas.read_csv("D:/highcharts/Volcanoes_USA.txt")
+lon=list(data["LON"])
+lat=list(data["LAT"])
+elev=list(data["ELEV"])
+def color_para(elevation):
+    if elevation<1000:
+        return "green"
+    elif 1000<=elevation<3000:
+        return "orange"
+    else:
+        return "red"
+map=folium.Map(location=[25.455923, 81.832704], tiles="OpenStreetMap ")
+fgv=folium.FeatureGroup(name="Volcanoes")
+for lt,ln,el in zip(lat,lon,elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln],radius=6,popup=str(el)+" m",
+    fill_color=color_para(el),color='grey',fill_opacity=0.7))
+fgp=folium.FeatureGroup(name="population")
+fgp.add_child(folium.GeoJson(data=open('D:/highcharts/world.json', 'r',encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("map1.html")
